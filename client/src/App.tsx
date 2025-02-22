f56bf1ffb321ba0c2fdeaf74a0bd776ed9a6f19e
bhandiwad <39637298-bhandiwad@users.noreply.replit.com>
2025-02-22 14:57:01 +0000

    Agent query: Can you see the chat interface with the welcome message? Try typing a message to check if it works.
    Replace all routes with MainChatInterface component.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/30c8edf8-fc0b-4faa-be59-155dc90b67d1/ee80cf5e-2462-49cd-8667-72f98c5501cc.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,28 +2,13 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import Login from "@/pages/login";
-import Dashboard from "@/pages/dashboard";
-import CreateExam from "@/pages/create-exam";
-import AddTemplate from "@/pages/add-template";
-import ManageTemplates from "@/pages/manage-templates";
-import EditTemplate from "@/pages/edit-template";
-import TakeExam from "@/pages/take-exam";
-import UploadAnswers from "@/pages/upload-answers";
-import NotFound from "@/pages/not-found";
+import { MainChatInterface } from "@/components/MainChatInterface";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/create" component={CreateExam} />
-      <Route path="/add-template" component={AddTemplate} />
-      <Route path="/manage-templates" component={ManageTemplates} />
-      <Route path="/edit-template/:id" component={EditTemplate} />
-      <Route path="/take/:id" component={TakeExam} />
-      <Route path="/upload" component={UploadAnswers} />
-      <Route component={NotFound} />
+      <Route path="/" component={MainChatInterface} />
+      <Route path="*" component={MainChatInterface} />
     </Switch>
   );
 }
@@ -31,8 +16,10 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
+      <div className="min-h-screen bg-background p-4">
+        <Router />
+        <Toaster />
+      </div>
     </QueryClientProvider>
   );
 }
